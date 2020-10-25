64a268307ad6e4df39ad089dc7c688ce167c2cf3
Roman <lytkin.roma00@mail.ru>
2020-10-25 22:25:18 +0300

    Реализованы Insert и Update

--- HW_3/orm.py
+++ HW_3/orm.py
@@ -8,6 +8,10 @@ SQLITE_TYPE_MAP = {
     bool: "INTEGER",  # 0 or 1
 }
 CREATE_TABLE_SQL = "CREATE TABLE {name} ({fields});"
+INSERT_SQL = "INSERT INTO {name} ({fields}) VALUES ({placeholders});"
+SELECT_ALL_SQL = "SELECT * FROM {name};"
+SELECT_WHERE_SQL = "SELECT * FROM {name} WHERE id=?;"
+UPDATE_SQL = "UPDATE {table_name} SET {query} WHERE id=?;"
 
 
 class Database:
@@ -16,18 +20,51 @@ class Database:
         self.conn = sqlite3.connect(path)
 
     def _execute(self, sql, params=None): # дописать тип params
+        print(sql, params)
         if params:
             self.conn.execute(sql, params)
         else:
             self.conn.execute(sql)
 
     def create(self, table):
-        print(table._get_create_sql())
         self._execute(table._get_create_sql())
+
+    def add(self, instance):
+        if instance._id is not None:
+            raise ValueError("Этот объект уже занесён в БД")
+        instance.__class__._size += 1
+        instance._id = instance.__class__._size
+        sql, values = instance._get_insert_sql()
+        self._execute(sql, values)
+        self.conn.commit()
+
+    def all(self, table):
+        result = []
+        sql = table._get_select_all_sql()
+        for row in self.conn.execute(sql):
+            result.append(row)
+        return result
+
+    def get(self, table, id):
+        result = []
+        sql = table._get_select_where_sql()
+        params = str(id)
+        for row in self.conn.execute(sql, params):
+            result.append(row)
+        return result
+
+    def update(self, instance):
+        sql = instance._get_update_sql()
+        self._execute(sql, (instance._id,))
+        self.conn.commit()
 
 
 
 class Table:
+    _size = 0
+
+    def __init__(self):
+        self._id = None
 
     @classmethod
     def _get_name(cls):
@@ -42,12 +79,57 @@ class Table:
         for name, field in inspect.getmembers(cls):
             if isinstance(field, Column):
                 fields.append((name, field.sql_type))
-            elif isinstance(field, ForeignKey):
-                fields.append((name + "_id", "INTEGER"))
 
         fields = [" ".join(x) for x in fields]
         return CREATE_TABLE_SQL.format(name=cls._get_name(),
                                        fields=", ".join(fields))
+
+    def _get_insert_sql(self):
+        cls = self.__class__
+        fields = []
+        placeholders = []
+        values = []
+
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+                values.append(getattr(self, name))
+                placeholders.append('?')
+
+        sql = INSERT_SQL.format(name=cls._get_name(),
+                                fields=", ".join(fields),
+                                placeholders=", ".join(placeholders))
+
+        return sql, values
+
+    @classmethod
+    def _get_select_all_sql(cls):
+        sql = SELECT_ALL_SQL.format(name=cls._get_name())
+        return sql
+
+    @classmethod
+    def _get_select_where_sql(cls):
+        sql = SELECT_WHERE_SQL.format(name=cls._get_name())
+        return sql
+
+    def _get_update_sql(self):
+        cls = self.__class__
+        query = []
+
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                if isinstance(getattr(self, name), str):
+                    query.append("{name}='{val}'".format(name=name, val=getattr(self, name)))
+                else:
+                    query.append("{name}={val}".format(name=name, val=getattr(self, name)))
+
+        sql = UPDATE_SQL.format(table_name=cls._get_name(),
+                                query=", ".join(query))
+
+        return sql
+
+
+
 
 class Column:
 
@@ -58,10 +140,6 @@ class Column:
     def sql_type(self):
         return SQLITE_TYPE_MAP[self.type]
 
-class ForeignKey:
-
-    def __init__(self, table):
-        self.table = table
 
 if __name__ == '__main__':
     pass
--- HW_3/test_orm.py
+++ HW_3/test_orm.py
@@ -1,5 +1,6 @@
+import os
 from orm import Database
-from orm import Table, Column, ForeignKey
+from orm import Table, Column
 
 DB_PATH = "./test.db"
 
@@ -7,12 +8,21 @@ class Author(Table):
     name = Column(str)
     lucky_number = Column(int)
 
-class Post(Table):
-    title = Column(str)
-    published = Column(bool)
-    author = ForeignKey(Author)
-
 if __name__ == '__main__':
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
     db = Database(DB_PATH)
     db.create(Author)
-    db.create(Post)
+
+    greg = Author()
+    greg.name = 'greg'
+    greg.lucky_number = 12
+    roma = Author()
+    roma.name = 'roma'
+    roma.lucky_number = 17
+    db.add(greg)
+    db.add(roma)
+    print(db.all(Author))
+    greg.lucky_number = 13
+    db.update(greg)
+    print(db.all(Author))
